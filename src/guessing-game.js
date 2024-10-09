423f2aaa69f1a284adacf3af2172b98858dd4421
Igor <106102873+Igorsk1@users.noreply.github.com>
2024-10-09 23:26:12 +0300

    fix: change line indentation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-			this.min = min;
-			this.max = max;
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-      return Math.round((this.min + this.max) / 2);
-		}
+        return Math.round((this.min + this.max) / 2);
+	}
 
     lower() {
-      this.max = this.guess();
+        this.max = this.guess();
     }
 
     greater() {
-      this.min = this.guess();
+        this.min = this.guess();
     }
 }
 
